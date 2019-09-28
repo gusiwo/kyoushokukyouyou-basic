148917e567f21d2a1391278e2b55ce809976d80a
gusiwo <54986855+gusiwo@users.noreply.github.com>
2019-09-28 23:10:22 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+$(function() {
+  // 「#login-show」要素に対するclickイベントを作成してください
+  $('#login-show').click(function(){
+    $('#login-modal').fadeIn();
+  });
+
+  $('.signup-show').click(function(){
+   $('#signup-modal').fadeIn();
+ })
+
+ $('.close-modal').click(function(){
+    $('#login-modal').fadeOut();
+    $('#signup-modal').fadeOut();
+  });
+
+//ページ内リンク
+$('#top-btn').click(function(){
+    $('html, body').scrollTop(0);
+  })
